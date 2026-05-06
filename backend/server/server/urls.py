cb2358e0a0240c52f56dc842ae2b9804d96fa89e
nicapretty <hontiverosacin@gmail.com>
2026-05-06 11:56:36 +0800

    endpoints models

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
 ]
+
+urlpatterns += endpoints_urlpatterns
